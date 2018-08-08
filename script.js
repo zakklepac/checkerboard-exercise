dff7f96f57f3a734dd392d666a449a902b526ce9
zakklepac <41202649+zakklepac@users.noreply.github.com>
2018-08-08 08:59:58 -0500

    red and black checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Your JS goes here
+let body = document.getElementsByTagName('body')[0];
+
+for (let i = 0; i < 81; i++) {
+  let squares = document.createElement('div');
+
+  squares.style.float = 'left';
+  squares.style.width = '11.1%';
+  squares.style.paddingBottom = '11.1%';
+
+  if (i % 2 === 0) {
+    squares.style.backgroundColor = 'red';
+  } else {
+    squares.style.backgroundColor = 'black';
+  }
+
+  body.appendChild(squares);
+}
